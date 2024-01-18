1c2cbaa651ca5bf08c5952665dca133139531ef0
IvxnG <gonzalez.martino.ivan@gmail.com>
2024-01-18 10:32:20 +0100

    registro en proceso

--- js/registro.js
+++ js/registro.js
@@ -1,34 +1,59 @@
-const url = 'http://localhost:3000/api/login';
+const url = 'http://localhost:3000/api/user/register';
 
-let loginForm = document.querySelector(".my-form");
-let btnLog = document.getElementById("btnLog");
+let createForm = document.querySelector(".my-form");
+let btnCreate = document.getElementById("btnCreate");
+let statusMsg;
 
-btnLog.addEventListener("click", createUser)
+btnCreate.addEventListener("click", createUser)
 
 function createUser(e) {
   e.preventDefault();
 
-  let username = document.getElementById("email");
-  let pass = document.getElementById("password");
+  // let username = document.getElementById("username");
+  // let user = document.getElementById("user");
+  // let pass = document.getElementById("pass");
+  // let email = document.getElementById("email");
 
-  if (username.value.length == 0) {
-    alert("Introduce el nombre de usuario")
+  let city = document.getElementById("city");
+  city = city.value.trim() || "Sin definir";
+
+  let club = document.getElementById("club");
+  club = club.value.trim() || "Sin definir";
+
+  let phone = document.getElementById("phone");
+  phone = phone.value.trim() || "Sin definir";
+
+
+  let userData = {
+    name: "username2",
+    username: "user1157",
+    mail: "email2",
+    pass: "pass",
+    city: city,
+    phone: phone,
+    club: club,
+    rol: "user",
   }
-  if (pass.value.length == 0) {
-    alert('Introduce la contraseña')
-  }
 
-  if (username.value.length != 0 && pass.value.length != 0) {
-    console.log(username.value);
+  let options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userData),
+  };
 
-    const options ={
-      method: 'POST',
-      body: JSON.stringify(userData)
-    };
 
-    fetch ( url , options )
-      .then( res => res.json)
-      .then( data => console.log(data))
-  }
-
+  fetch(url, options)
+    .then(res => {
+      if (res.status == 200) {
+        alert("Registrado con éxito!!")
+      }
+      return res.json();
+    })
+    .then(response => {
+      if ( response.id ){
+        console.log(response.id);
+      } else {
+        console.log(response.msg);
+      }
+    })
 }
