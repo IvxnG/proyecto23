20f35ee52136ddaa7db6d9ba63e5bd188fc4bac8
ivxng <gonzalez.martino.ivan@gmail.com>
2024-01-21 12:43:19 +0100

    login y registro, añadido js

--- js/login.js
+++ js/login.js
@@ -1,42 +1,54 @@
+// Definición de la URL del servidor para la autenticación
 const url = 'http://localhost:3000/api/auth/login';
 
+// Obtención de referencias a elementos del DOM
 let loginForm = document.querySelector(".my-form");
 let btnLog = document.getElementById("btnLog");
 let btnToken = document.getElementById("btnToken");
 let btnHome = document.getElementById("btnHome");
+let passIcon = document.getElementById("passIcon");
+let pass = document.getElementById("password");
 
-btnLog.addEventListener("click", logUser)
-btnToken.addEventListener("click", borrarToken)
+// Asignación de eventos a los elementos del formulario
+passIcon.addEventListener("click", showHidePass);
+btnLog.addEventListener("click", logUser);
+btnToken.addEventListener("click", borrarToken);
+btnHome.addEventListener("click", home);
+
+// Función para borrar el token almacenado en el localStorage
 function borrarToken() {
   console.log("Borrado el token");
-  localStorage.removeItem("token")
-}
-btnHome.addEventListener("click", home);
-function home() {
-  location.href = '../index.html'
+  localStorage.removeItem("token");
 }
 
+// Función para redirigir a la página principal
+function home() {
+  location.href = '../index.html';
+}
+
+// Función para manejar el inicio de sesión del usuario
 function logUser(e) {
   e.preventDefault();
-
   let username = document.getElementById("user");
-  let pass = document.getElementById("password");
 
+  // Validación para asegurarse de que se ingresen tanto el nombre de usuario como la contraseña
   if (username.value.length == 0) {
-    alert("Introduce el nombre de usuario")
+    alert("Introduce el nombre de usuario");
   }
   if (pass.value.length == 0) {
-    alert('Introduce la contraseña')
+    alert('Introduce la contraseña');
   }
 
+  // Si ambos campos tienen datos, realiza la solicitud de inicio de sesión
   if (username.value.length != 0 && pass.value.length != 0) {
-    console.log("-----------");
 
+    // Crea un objeto con los datos del usuario
     let userData = {
-      username: user.value,
-      pass: pass.value,
+      username: user.value.trim(),
+      pass: pass.value.trim(),
     }
 
+    // Configuración de las opciones para la solicitud fetch
     let options = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -45,23 +57,33 @@ function logUser(e) {
 
     console.log(userData);
 
+    // Realiza la solicitud POST para la autenticación del usuario
     fetch(url, options)
       .then(res => {
         if (res.status == 200) {
           return res.json()
             .then(response => {
               console.log(response);
+              // Almacena el token en el localStorage y redirige a la página principal
               localStorage.setItem("token", response.token);
               location.href = '../index.html';
             })
         }
+        // Maneja el caso en el que las credenciales no son válidas
         if (res.status == 401) {
-          alert("Credenciales no válidas");
+          alert("Credenciales no válidas.");
         }
-        ;
       })
-
   }
 }
 
+// Función para mostrar u ocultar la contraseña
+function showHidePass(e) {
+  e.preventDefault();
 
+  if (pass.type == "password") {
+    pass.type = "text";
+  } else {
+    pass.type = "password"
+  }
+}
--- js/registro.js
+++ js/registro.js
@@ -1,58 +1,140 @@
+// Definición de la URL del servidor para creae el usuario
 const url = 'http://localhost:3000/api/user/register';
 
+// Selecciona elementos del DOM
 let createForm = document.querySelector(".my-form");
 let btnCreate = document.getElementById("btnCreate");
+let passIcon = document.getElementById("passIcon");
 
-btnCreate.addEventListener("click", createUser)
+let user = document.getElementById("user");
+let username = document.getElementById("username");
+let pass = document.getElementById("password");
+let email = document.getElementById("email");
 
+// Variables de validación
+let validUser;
+let validUsername;
+let validPass;
+let validEmail;
+
+// Añade listeners de eventos a elementos del formulario
+user.addEventListener("blur", checkUser);
+username.addEventListener("blur", checkUsername);
+email.addEventListener("blur", checkEmail);
+pass.addEventListener("blur", checkPass);
+
+passIcon.addEventListener("click", showHidePass);
+btnCreate.addEventListener("click", createUser);
+
+// Función para crear el usuario
 function createUser(e) {
   e.preventDefault();
 
-  // let username = document.getElementById("username");
-  // let user = document.getElementById("user");
-  // let pass = document.getElementById("pass");
-  // let email = document.getElementById("email");
+  // Obtiene valores de campos adicionales o establece un valor predeterminado si están vacíos
+  let city = document.getElementById("city").value.trim() || "Sin definir";
+  let club = document.getElementById("club").value.trim() || "Sin definir";
+  let phone = document.getElementById("phone").value.trim() || "Sin definir";
 
-  let city = document.getElementById("city");
-  city = city.value.trim() || "Sin definir";
-
-  let club = document.getElementById("club");
-  club = club.value.trim() || "Sin definir";
-
-  let phone = document.getElementById("phone");
-  phone = phone.value.trim() || "Sin definir";
-
-
+  // Crea un objeto con los datos del usuario
   let userData = {
-    name: "ivan",
-    username: "ivan",
-    mail: "emai",
-    pass: "1234",
+    name: user.value.trim(),
+    username: username.value.trim(),
+    mail: email.value.trim(),
+    pass: pass.value.trim(),
     city: city,
     phone: phone,
     club: club,
     rol: "user",
   }
 
+  // Configura las opciones para la solicitud fetch
   let options = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(userData),
   };
 
+  // Realiza la solicitud POST solo si todos los campos son válidos
+  if (validUser && validUsername && validPass && validEmail) {
+    fetch(url, options)
+      .then(res => {
+        if (res.status == 200) {
+          alert("Registrado con éxito!!");
+          location.href = "login.html";
+        }
+        return res.json();
+      })
+      .then(response => {
+        if (response.id) {
+          console.log(response);
+        } else {
+          console.log(response.msg);
+        }
+      })
+  }
+}
 
-  fetch(url, options)
-    .then(res => {
-      if (res.status == 200) {
-        alert("Registrado con éxito!!")
-      }
-      return res.json();
-    })
-    .then(response => {
-      if ( response.id ){
-        console.log(response);
-      } else {
-        console.log(response.msg);
-      }
-    })
+// Mostrar y ocultar la contraseña
+function showHidePass(e) {
+  e.preventDefault();
+
+  // Cambia el tipo de entrada del campo de contraseña para mostrar u ocultar la contraseña
+  if (pass.type == "password") {
+    pass.type = "text";
+  } else {
+    pass.type = "password"
+  }
+}
+
+// Funciones para comprobar user, username, pass y email
+function checkUser(e) {
+  e.preventDefault();
+
+  // Verifica si el valor del campo de usuario cumple con la expresión regular
+  if (!(/^[a-zA-Z\s']+$/.test(user.value.trim()))) {
+    document.getElementById("userIcon").setAttribute("stroke", "red")
+    validUser = false;
+  } else {
+    document.getElementById("userIcon").setAttribute("stroke", "currentColor")
+    validUser = true;
+  }
+}
+
+function checkUsername(e) {
+  e.preventDefault();
+
+  // Verifica si el valor del campo de nombre de usuario cumple con la expresión regular
+  if (!(/^[a-zA-Z0-9_]{3,16}$/.test(username.value.trim()))) {
+    document.getElementById("usernameIcon").setAttribute("stroke", "red")
+    validUsername = false;
+  } else {
+    document.getElementById("usernameIcon").setAttribute("stroke", "currentColor")
+    validUsername = true;
+  }
+}
+
+function checkEmail(e) {
+  e.preventDefault();
+
+  // Verifica si el valor del campo de correo electrónico cumple con la expresión regular
+  if (!(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email.value.trim()))) {
+    document.getElementById("mailIcon").setAttribute("stroke", "red")
+    validEmail = false;
+  } else {
+    document.getElementById("mailIcon").setAttribute("stroke", "currentColor")
+    validEmail = true;
+  }
+}
+
+function checkPass(e) {
+  e.preventDefault();
+
+  // Verifica si el valor del campo de contraseña cumple con la expresión regular
+  if (!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[a-zA-Z\d\W_]{10,}$/.test(pass.value.trim()))) {
+    document.getElementById("passIcon").setAttribute("stroke", "red")
+    validPass = false;
+  } else {
+    document.getElementById("passIcon").setAttribute("stroke", "currentColor")
+    validPass = true;
+  }
 }
