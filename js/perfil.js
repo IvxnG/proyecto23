ef61638afe21ad6995f3569d6b7eef6dd5a8a16d
IvxnG <gonzalez.martino.ivan@gmail.com>
2024-01-26 14:37:47 +0100

    editando perfil

--- js/perfil.js
+++ js/perfil.js
@@ -1,7 +1,30 @@
-
+const url = `http://localhost:3000/api/user/username/${localStorage.getItem("user")}`;
+let data;
 if (localStorage.getItem("token")) {
 
+    let options = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      };
+
+    fetch(url, options)
+    .then(res => {
+      if (res.status == 200) {
+        return res.json()
+          .then(response => {
+            
+            data = response
+            console.log(data);
+          })
+      }
+      // Maneja el caso en el que no existe el usuario
+      if (res.status == 404) {
+        alert("Usuario no existente!");
+        location.href = "../index.html";
+      }
+    })
 }
+console.log(data);
 
 let btnEdit = document.getElementById("btnEdit");
 let btnCancel = document.getElementById("btnCancel");
@@ -11,7 +34,6 @@ btnEdit.addEventListener("click", editPerfil);
 
 function goIndex(e) {
     e.preventDefault();
-
     location.href = "../index.html";
 }
 
