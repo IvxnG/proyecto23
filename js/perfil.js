f8e7a0ac3a99664bc153a365cc15c8bce768d2ea
ivxng <gonzalez.martino.ivan@gmail.com>
2024-01-28 13:21:22 +0100

    corfirmacion borrar cuenta

--- js/perfil.js
+++ js/perfil.js
@@ -1,43 +1,154 @@
-const url = `http://localhost:3000/api/user/username/${localStorage.getItem("user")}`;
-let data;
+// Define la URL para la solicitud GET utilizando el username almacenado en localStorage y otras URL
+const url = `http://localhost:3000/api/user/username/${localStorage.getItem("username")}`;
+const urlCheck = 'http://localhost:3000/api/user/check'
+const urlDelete = `http://localhost:3000/api/user/username/${localStorage.getItem("username")}`;
+
+// Verifica si hay un token en localStorage antes de realizar la solicitud
 if (localStorage.getItem("token")) {
 
-    let options = {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      };
+  // Configura las opciones para la solicitud fetch
+  let options = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  };
 
-    fetch(url, options)
+  // Realiza la solicitud fetch para rellenar con datos del usuario
+  fetch(url, options)
     .then(res => {
       if (res.status == 200) {
+        // Procesa la respuesta JSON y actualiza los campos en la página
         return res.json()
           .then(response => {
-            
-            data = response
-            console.log(data);
-          })
+            document.getElementById("user").value = response.user;
+            document.getElementById("username").value = response.username;
+            document.getElementById("email").value = response.mail;
+            document.getElementById("password").value = response.pass;
+            document.getElementById("city").value = response.city || "Sin definir";
+            document.getElementById("phone").value = response.phone || "Sin definir";
+            document.getElementById("club").value = response.club || "Sin definir";
+          });
       }
-      // Maneja el caso en el que no existe el usuario
+      // Maneja el caso en el que no existe el usuario (código 404)
       if (res.status == 404) {
         alert("Usuario no existente!");
         location.href = "../index.html";
       }
-    })
+    });
+} else {
+  //redirige a la landing page si no esta logeado
+  location.href = "../index.html";
 }
-console.log(data);
 
+//Guardo los dos formularios
+let formEdit = document.getElementById("formEdit");
+let formDelete = document.getElementById("formDelete");
+
+// Obtiene referencias a los botones de la interfaz
 let btnEdit = document.getElementById("btnEdit");
 let btnCancel = document.getElementById("btnCancel");
+let btnToken = document.getElementById("btnToken");
+let btnDelete = document.getElementById("btnDelete");
+let btnConfirmDelete = document.getElementById("btnConfirmDelete");
+let btnCancelDelete = document.getElementById("btnCancelDelete");
 
-btnCancel.addEventListener("click", goIndex);
+// Asigna eventos a los botones
 btnEdit.addEventListener("click", editPerfil);
+btnCancel.addEventListener("click", goIndex);
+btnToken.addEventListener("click", closeSesion);
+btnDelete.addEventListener("click", showDeleteForm);
+btnCancelDelete.addEventListener("click", showDeleteForm);
+btnConfirmDelete.addEventListener("click", deleteUser);
 
+//Icono para mostrar y ocultar la contraseña
+let passIcon = document.getElementById("passIcon");
+let pass = document.getElementById("password");
+passIcon.addEventListener("click", showHidePass);
+
+// Función para redirigir a la página de inicio
 function goIndex(e) {
-    e.preventDefault();
-    location.href = "../index.html";
+  e.preventDefault();
+  location.href = "../index.html";
 }
 
+// Función para editar los datos del usuario, comprueba que el username no exita, por si lo cambia
 function editPerfil(e) {
-    e.preventDefault();
-    console.log("aqui edito");
+  e.preventDefault();
+  console.log("aqui edito");
+
+  //Comprueba que los datos introducidos cumplen las expresiones regulares
+  if (document.getElementById("username").value.trim() != "" || document.getElementById("username").value.trim() == localStorage.getItem("username")) {
+    // Configura las opciones para la solicitud fetch de comprobar el nombre
+    let optionsCheck = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "username": document.getElementById("username").value.trim() }),
+    };
+
+    //Comprueba si el nombre esta disponible para crear la cuenta
+    fetch(urlCheck, optionsCheck)
+      .then(response => {
+        if (response.status == 200) {
+          //Si esta disponible hace el fetch para crear la cuenta
+          console.log("Disponible");
+        }
+        if (response.status == 409) {
+          console.log("Nombre de usuario en uso!")
+        }
+      })
+  } else {
+    console.log("test");
+  }
+
+}
+
+function showDeleteForm(e){
+  e.preventDefault();
+  console.log("object");
+
+  formEdit.classList.toggle("hidden");
+  formDelete.classList.toggle("hidden");
+}
+
+//Borrar la cuenta del usurio (debe tener sesion iniciada)
+function deleteUser(e) {
+  e.preventDefault();
+  console.log("borrar user e ir index");
+
+  fetch(urlDelete)
+    .then(res => {
+      console.log(res);
+      // Maneja la respuesta del servidor
+      if (res.status == 200) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("username");
+        alert("Cuenta borrada con éxito!");
+        //location.href = "../index.html";
+      }
+      // Maneja el caso en el que no existe el usuario (código 404)
+      if (res.status == 404) {
+        alert("Usuario no existente!");
+        location.href = "../index.html";
+      }
+    });
+}
+
+// Mostrar y ocultar la contraseña
+function showHidePass(e) {
+  e.preventDefault();
+
+  // Cambia el tipo de entrada del campo de contraseña para mostrar u ocultar la contraseña
+  if (pass.type == "password") {
+    pass.type = "text";
+  } else {
+    pass.type = "password"
+  }
+}
+
+// Función para borrar el token almacenado en el localStorage y cerrar sesion
+function closeSesion(e) {
+  e.preventDefault();
+  console.log("Sesion cerrarda!");
+  localStorage.removeItem("token");
+  localStorage.removeItem("username");
+  location.href = '../index.html';
 }
